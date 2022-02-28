0fe593732fcb7289cf7a0bb72aedb14ecad89d6b
shabieh2 <shabieh2@gmail.com>
2022-02-28 15:34:35 -0800

    Update main.py

--- main.py
+++ main.py
@@ -163,7 +163,7 @@ if is_valid:
             st.warning('Video playback not available on deployed version due to licensing restrictions. ')
             with st.spinner(text='Preparing Video'):
                 for vid in os.listdir(_get_latest_folder()):
-                    #os.system('ffmpeg -i f'{_get_latest_folder()}/{vid}' -vcodec libx264 f'{_get_latest_folder()}/{vid}'')
+                    os.system('ffmpeg -i f'{_get_latest_folder()}/{vid}' -vcodec libx264 f'{_get_latest_folder()}/{vid}'')
                     st.video(f'{_get_latest_folder()}/{vid}')
                 st.balloons()
         else:
